db9fdb29c542eafcd675ec03ce3494e646980541
bavtim <40119744+bavtim@users.noreply.github.com>
2020-05-31 07:42:12 +0300

    fix

--- core/src/com/gg/game/Bootscreen.java
+++ core/src/com/gg/game/Bootscreen.java
@@ -3,92 +3,80 @@ package com.gg.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-
-import static java.lang.Thread.sleep;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 
 public class Bootscreen implements Screen {
-    MyGdxGame menu;
+    private Sprite sprite;
+    private float timer = 0f, timer1 = 0f;
+    private boolean flag = false, flag1 = false;
+    private BitmapFont font;
+    private Texture Bootscreen_IMG;
+    private float deltatime;
+    Bootscreen() {
+    }
 
-
-
-    public Bootscreen(){ }
-
-
-
-    Sprite sprite;
-
-    float timer =0f,timer1 =0f;
-    boolean flag=false,flag1=false;
-    BitmapFont font;
-    Texture Bootscreen_IMG;
-    float deltatime=0;
     @Override
     public void show() {
         Bootscreen_IMG = new Texture("Bootscreen/joystick.png");
-
-        sprite = new Sprite(Bootscreen_IMG,0,0,512,512);
-        sprite.setPosition(Gdx.graphics.getWidth()/2-256, Gdx.graphics.getHeight()/2-256);
+        sprite = new Sprite(Bootscreen_IMG, 0, 0, 512, 512);
+        sprite.setPosition(Gdx.graphics.getWidth() / 2f - 256, Gdx.graphics.getHeight() / 2f - 256);
         font = new BitmapFont();
-        font.setColor(0,0,0,0);
-        font.getData().setScale(Gdx.graphics.getWidth()/100);
-        sprite.setColor(0,0,0,0);
+        font.setColor(0, 0, 0, 0);
+        font.getData().setScale(Gdx.graphics.getWidth() / 100f);
+        sprite.setColor(0, 0, 0, 0);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        deltatime=Gdx.graphics.getDeltaTime();
-        if(deltatime<1){
-            if(flag){
-                if(timer>0)
-                timer -=deltatime;
-            }else{
-                timer +=deltatime;
+        deltatime = Gdx.graphics.getDeltaTime();
+        if (deltatime < 1) {
+            if (flag) {
+                if (timer > 0)
+                    timer -= deltatime;
+            } else {
+                timer += deltatime;
             }
-            if(timer>2)
-                flag=true;
-            if(timer<0){
-                if(flag1){
-                    timer1 -=deltatime;
+            if (timer > 2)
+                flag = true;
+            if (timer < 0) {
+                if (flag1) {
+                    timer1 -= deltatime;
                 }
-                if(!flag1){
-                    timer1 +=deltatime;
-                    if(timer1>2)
-                        flag1=true;
+                if (!flag1) {
+                    timer1 += deltatime;
+                    if (timer1 > 2)
+                        flag1 = true;
                 }
 
-                if(timer1<0)
-                   timer1=0;
+                if (timer1 < 0)
+                    timer1 = 0;
             }
 
         }
 
 
-        sprite.setColor(0,0,0, timer);
+        sprite.setColor(0, 0, 0, timer);
 
         MyGdxGame.camera.update();
-        menu.batch.setProjectionMatrix(menu.camera.combined);
-        font.setColor(0,0,0,timer1);
-        menu.batch.begin();
-        font.draw(menu.batch,"GGgame",Gdx.graphics.getWidth()/2-Gdx.graphics.getWidth()/3.5f,Gdx.graphics.getHeight()/2+Gdx.graphics.getWidth()/10f,0,40, false);
-        sprite.draw(menu.batch);
-        menu.batch.end();
+        MyGdxGame.batch.setProjectionMatrix(MyGdxGame.camera.combined);
+        font.setColor(0, 0, 0, timer1);
+        MyGdxGame.batch.begin();
+        font.draw(MyGdxGame.batch, "GGgame", Gdx.graphics.getWidth() / 2f - Gdx.graphics.getWidth() / 3.5f, Gdx.graphics.getHeight() / 2f+ Gdx.graphics.getWidth() / 10f, 0, 40, false);
+        sprite.draw(MyGdxGame.batch);
+        MyGdxGame.batch.end();
 
     }
 
     @Override
     public void resize(int width, int height) {
-        MyGdxGame.camera.setToOrtho(false,Gdx.graphics.getWidth() , Gdx.graphics.getHeight());
-        sprite.setPosition(Gdx.graphics.getWidth()/2-256, Gdx.graphics.getHeight()/2-256);
-        font.getData().setScale(Gdx.graphics.getWidth()/100);
+        MyGdxGame.camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        sprite.setPosition(Gdx.graphics.getWidth() / 2f - 256, Gdx.graphics.getHeight() / 2f - 256);
+        font.getData().setScale(Gdx.graphics.getWidth() / 100f);
     }
 
     @Override
